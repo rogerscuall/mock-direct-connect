05bc20a670405ca8079684f3729af721de68ff35
Roger Gomez <rgomez@presidio.com>
2023-04-11 19:16:34 -0400

    Cleanup config.

--- terraform-aws-direct-connect/main.tf
+++ terraform-aws-direct-connect/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
   }
 }
 
-resource "aws_dx_connection" "hoge" {
+resource "aws_dx_connection" "this" {
   name      = "tfx-connection-exmple"
   location  = "EqSe2"
   bandwidth = "1Gbps"
@@ -24,20 +24,19 @@ resource "aws_dx_connection" "hoge" {
   }
 }
 
-resource "aws_dx_gateway" "example" {
+resource "aws_dx_gateway" "this" {
   name            = "tf-dxg-exampl123"
   amazon_side_asn = "64512"
 }
 
-
-resource "aws_dx_private_virtual_interface" "foo" {
-  connection_id = aws_dx_connection.hoge.id
+resource "aws_dx_private_virtual_interface" "this" {
+  connection_id = aws_dx_connection.this.id
 
   name             = "vif-foo"
   vlan             = 4094
   address_family   = "ipv4"
   bgp_asn          = 65352
-  dx_gateway_id    = aws_dx_gateway.example.id
+  dx_gateway_id    = aws_dx_gateway.this.id
   customer_address = "10.0.0.1/28"
   amazon_address   = "10.0.0.2/28"
   bgp_auth_key = "1234567890"
@@ -47,13 +46,13 @@ resource "aws_dx_private_virtual_interface" "foo" {
 
 }
 
-resource "aws_dx_public_virtual_interface" "foo" {
-  connection_id = aws_dx_connection.hoge.id
+resource "aws_dx_public_virtual_interface" "this" {
+  connection_id = aws_dx_connection.this.id
 
   name           = "vif-foo"
-  vlan           = 4094
+  vlan           = 4090
   address_family = "ipv4"
-  bgp_asn        = 65352
+  bgp_asn        = 65350
 
   customer_address = "175.45.176.1/30"
   amazon_address   = "175.45.176.2/30"
@@ -62,4 +61,14 @@ resource "aws_dx_public_virtual_interface" "foo" {
     "210.52.109.0/24",
     "175.45.176.0/22",
   ]
+}
+
+data "aws_dx_connection" "this" {
+  name = "tfx-connection-exmple"
+}
+
+resource "aws_dx_bgp_peer" "this" {
+  virtual_interface_id = aws_dx_private_virtual_interface.this.id
+  address_family       = "ipv4"
+  bgp_asn              = 65351
 }
