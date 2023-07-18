185e26ff18c7f437515282e6c4e0a3269037df41
Roger Gomez <rgomez@presidio.com>
2023-07-17 20:14:50 -0400

    cleanup.

--- examples/terraform-aws-direct-connect/main.tf
+++ examples/terraform-aws-direct-connect/main.tf
@@ -40,17 +40,6 @@ resource "aws_dx_bgp_peer" "peer" {
   amazon_address       = "1.1.1.1"
 }
 
-resource "aws_dx_bgp_peer" "peer1" {
-  virtual_interface_id = aws_dx_transit_virtual_interface.example.id
-  address_family       = "ipv4"
-  bgp_asn              = 65002
-  # This address is the remote address.
-  customer_address     = "74.235.253.86"
-  bgp_auth_key         = "1234567890"
-  # This address is ignored, use the address of the host running the mock server
-  amazon_address       = "1.1.1.1"
-}
-
 resource "aws_dx_transit_virtual_interface" "example" {
   connection_id = aws_dx_connection.this.id
 
@@ -74,41 +63,33 @@ resource "aws_dx_gateway_association" "example" {
   ]
 }
 
-# resource "aws_dx_private_virtual_interface" "this" {
-#   connection_id = aws_dx_connection.this.id
-
-#   name             = "vif-foo"
-#   vlan             = 4094
-#   address_family   = "ipv4"
-#   bgp_asn          = 65002
-#   dx_gateway_id    = aws_dx_gateway.this.id
-#   amazon_address       = "1.1.1.1"
-#   # customer_address     = "20.127.114.9"
-#   # bgp_auth_key = "1234567890"
+# resource "aws_dx_connection" "this1" {
+#   name      = "tfx-connection-exmple"
+#   location  = "EqSe2"
+#   bandwidth = "1Gbps"
 #   tags = {
-#     "pvif" = "one"
+#     Test = "tfx-connection-exmple"
 #   }
-
 # }
 
-# resource "aws_dx_public_virtual_interface" "this" {
-#   connection_id = aws_dx_connection.this.id
+# resource "aws_dx_bgp_peer" "peer1" {
+#   virtual_interface_id = aws_dx_transit_virtual_interface.example1.id
+#   address_family       = "ipv4"
+#   bgp_asn              = 65002
+#   # This address is the remote address.
+#   customer_address     = "74.235.253.86"
+#   bgp_auth_key         = "1234567890"
+#   # This address is ignored, use the address of the host running the mock server
+#   amazon_address       = "1.1.1.1"
+# }
 
-#   name           = "vif-foo"
-#   vlan           = 4090
+# resource "aws_dx_transit_virtual_interface" "example1" {
+#   connection_id = aws_dx_connection.this1.id
+
+#   dx_gateway_id  = aws_dx_gateway.this.id
+#   name           = "tf-transit-vif-example1"
+#   vlan           = 4094
 #   address_family = "ipv4"
-#   bgp_asn        = 65350
-
-#   customer_address = "175.45.176.1/30"
-#   amazon_address   = "175.45.176.2/30"
-
-#   route_filter_prefixes = [
-#     "210.52.109.0/24",
-#     "175.45.176.0/22",
-#   ]
-# }
-
-# This one can be used once the aws_dx_connection resource is available
-# data "aws_dx_connection" "this" {
-#   name = "tfx-connection-exmple"
+#   bgp_asn        = 65005
+#   mtu           = 1500
 # }
